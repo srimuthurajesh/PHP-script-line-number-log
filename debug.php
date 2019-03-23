894373083ab0efea4c3c34cb2a437ed39292db57
srimuthurajesh <srimuthurajesh@gmail.com>
2019-03-23 20:10:56 +0530

    Update debug.php

--- debug.php
+++ debug.php
@@ -1,10 +1,15 @@
 <?php
 
+/*Register a function for execution on each tick*/
 register_tick_function(function(){
+   
+    //check whether the file is current debug file
+    if ($file == __FILE__) return;
+   
     $backtrace = debug_backtrace();
     $line = $backtrace[0]['line'];
     $file = $backtrace[0]['file'];
-    //check whether the file is current debug file
-    if ($file == __FILE__) return;
+    
+    /*printing executed line filname & line*/
     echo "in $file on line $line\n<br>";
 });
